1119657995811966a1ad9e09e3248c19d745aa59
rongkaixia <rongkaixia@gmail.com>
2016-03-28 22:50:03 +0800

    update final code

--- code/mvn_project/my-app/src/main/scala/test.scala
+++ code/mvn_project/my-app/src/main/scala/test.scala
@@ -15,6 +15,10 @@ import breeze.numerics._
 import breeze.stats.{mean, stddev}
 
 object Stat {
+    def test(A: DenseMatrix[Double]): Double = {
+        val _mean = mean(A(::,*)).toDenseVector
+        return 1.0
+    }
     // each row of A is an observation
     def normalization(A: DenseMatrix[Double]): DenseMatrix[Double] = {
         val _mean = mean(A(::,*)).toDenseVector
@@ -68,15 +72,16 @@ object Context{
 }
 
 object Utils{
-    def CassandraTableScanRDD2DoubleArray(table: com.datastax.spark.connector.rdd.CassandraTableScanRDD[CassandraRow]): Array[Array[Double]] = {
-        return table.map(row => {
+    def cassandraTableScanRDD2DoubleArray(table: com.datastax.spark.connector.rdd.CassandraTableScanRDD[CassandraRow], rowSize: Int): DenseMatrix[Double] = {
+        var tmp = table.map(row => {
                 var ret = new Array[Double](row.size)
                 var i = 0
                 for(i <- 0 to row.size - 1){
                     ret(i) = row.getDouble(i)
                 }
                 ret
-            }).collect
+            }).reduce((a: Array[Double],b: Array[Double]) => a ++ b)
+        return new DenseMatrix[Double](tmp.size/rowSize, rowSize, tmp)
     }
 }
 
@@ -85,14 +90,11 @@ object SearchEngine{
     //     var ret = new Array[Double](row.size)
     // }
 
-    def doMatch(cass:com.datastax.spark.connector.rdd.CassandraTableScanRDD[CassandraRow], row: org.apache.spark.sql.Row): Int = {
-        return 1
+    def doMatch(A: DenseMatrix[Double], B: DenseMatrix[Double]): Double = {
+        var ret = Stat.brownianCorrelation(A,B)
+        return ret
     }
 
-    def doMatch2(cass:com.datastax.spark.connector.rdd.CassandraTableScanRDD[CassandraRow]): Int = {
-        cass.first
-        return 1
-    }
 }
 
 /** Computes an approximation to pi */
@@ -104,25 +106,26 @@ object App{
     val sc = new SparkContext(conf)
     Context.sparkContext = sc
     Context.sqlContext = new SQLContext(sc)
-    // Context.cassandraContext = new CassandraSQLContext(sc)
-    // val cass = sc.cassandraTable("chinamarket", "daymarketdata")
-    // val df = Context.cassandraContext.sql("SELECT * from chinamarket.daymarketquery100 limit 1000")
-    val tmp = new DenseMatrix(3,2, Array(1.0,2,3,4,5,6))
-    val tmp2 = pow(tmp,2)
-    val tmp3 = sum(tmp2)
-    println("==============")
-    println(Stat.brownianCorrelation(tmp,tmp))
+    Context.cassandraContext = new CassandraSQLContext(sc)
+    val cass = sc.cassandraTable("chinamarket", "daymarketdata")
+    // val tmp = new DenseMatrix(3,2, Array(1.0,2,3,4,5,6))
+    // println(Stat.brownianCorrelation(tmp,tmp))
     // Step 1 - 读取所有Sample数据
-    // val sampleIndexArray = df.collect()
-    // val sampleArray = sampleIndexArray.map(row => {
-    //     cass.select("open", "high", "low", "close")
-    //     .where("sid = ?", row.getInt(row.fieldIndex("sid")))
-    //     .where("datetime >= ?", row.getTimestamp(row.fieldIndex("datetime")))
-    //     .limit(100)
-    //     })
-    // val tmp = sampleArray.map(Utils.CassandraTableScanRDD2DoubleArray)
-    // println(tmp)
+    val df = Context.cassandraContext.sql("SELECT * from chinamarket.daymarketquery100 limit 200")
+    val sampleIndexArray = df.collect()
+    val sampleArray = sampleIndexArray.map(row => {
+        cass.select("open", "high", "low", "close")
+        .where("sid = ?", row.getInt(row.fieldIndex("sid")))
+        .where("datetime >= ?", row.getTimestamp(row.fieldIndex("datetime")))
+        .limit(10)
+        })
+    // val sampleArray = sampleIndexArray.map(row => {cass.select("open", "high", "low", "close").where("sid = ?", row.getInt(row.fieldIndex("sid"))).where("datetime >= ?", row.getTimestamp(row.fieldIndex("datetime"))).limit(10)})
+    val tmp = sampleArray.map(Utils.cassandraTableScanRDD2DoubleArray(_, 4))
     // Step 2 - 分发任务，计算brownianCorrelation
+    val query = tmp(0) // 用第一个作为query样本，剩下的为数据集
+    val smapleRDD = sc.parallelize(tmp.slice(1,tmp.size))
+    val result = smapleRDD.map(SearchEngine.doMatch(query, _)).collect()
+    result.foreach(println)
     println("Done!")
     sc.stop()
   }
